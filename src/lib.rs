f02ef9a95eea8ec5e6a282ef519dd67683ca123a
Joshua J. Bouw <dev@joshuajbouw.com>
2021-02-20 13:00:02 +0700

    feat: add NetworkStatus

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
 pub fn it_works() -> bool {
     true
+
+#[derive(Clone)]
+pub struct NetworkStatus {
+    pub connected_peers: usize,
+    pub active_peers: usize,
+    pub total_bytes_inbound: u64,
+    pub total_bytes_outbound: u64,
 }
